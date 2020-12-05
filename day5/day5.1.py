b25ededd5308462bab487bdb44a10f4b86794952
ThePoleThatFishes <63863162+ThePoleThatFishes@users.noreply.github.com>
2020-12-05 15:52:29 +0200

    Create day5.1.py

---
+++ day5/day5.1.py
@@ -0,0 +1,23 @@
+with open("day5.1_data.txt", "r") as f:
+    maxID = 0
+    seatID = 0
+    for bp in f:
+        minrow = 0
+        maxrow = 127
+        mincol = 0
+        maxcol = 7
+        for char in bp:
+            distrow = maxrow - minrow + 1
+            distcol = maxcol - mincol + 1
+            if char == "F":
+                maxrow -= distrow//2
+            elif char == "B":
+                minrow += distrow//2
+            elif char == "L":
+                mincol += distcol//2
+            elif char == "R":
+                maxcol -= distcol//2
+
+        seatID = minrow*8 + mincol
+        maxID = max(seatID, maxID)
+    print(maxID)
